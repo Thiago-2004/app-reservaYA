4795f921c3fa223eb2e32463552a465ebb779519
thiago-ricon <thiagoricon987@gmail.com>
2025-07-28 23:09:54 -0300

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/app-reservaYA/',
- // 👈 Agregá esta línea
-});
+  // ❌ base eliminado porque en Vercel no hace falta
+})
