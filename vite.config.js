16e67ea8784c1fc54c1873f3f3bf6b182f9ba55b
thiago-ricon <thiagoricon987@gmail.com>
2025-07-28 22:40:13 -0300

    ....

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './app-reservaYA', // 👈 Agregá esta línea
+  base: '/app-reservaYA/',
+ // 👈 Agregá esta línea
 });
